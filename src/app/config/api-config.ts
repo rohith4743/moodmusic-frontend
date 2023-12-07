71ff5f6230893e114e87d0ee85a791813d167b36
rohith4743 <rohithkankipati4743@gmail.com>
2023-12-06 19:22:03 -0500

    backend uri

--- src/app/config/api-config.ts
+++ src/app/config/api-config.ts
@@ -1,3 +1,3 @@
 
 // export const apiUrl = 'http://127.0.0.1:8000';  
-export const apiUrl = 'http://moodmusicbackend-env.eba-ju5w7dff.us-east-1.elasticbeanstalk.com/moodmusic/';  // Replace with your actual API URL
+export const apiUrl = 'http://moodmusicbackend-env.eba-ju5w7dff.us-east-1.elasticbeanstalk.com';  // Replace with your actual API URL
